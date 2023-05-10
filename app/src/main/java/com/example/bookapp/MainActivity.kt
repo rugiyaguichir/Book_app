a6c64865a504013ec38c83d70bfe220803085e2b
Rugiya <guichir_r@iuca.kg>
2023-05-10 14:08:51 +0600

    *

---
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.bookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener{
+
+        }
+
+        binding.skipBtn.setOnClickListener{
+
+        }
+    }
+}
